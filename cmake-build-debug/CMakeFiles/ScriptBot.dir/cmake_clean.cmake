bbe609120c69bc32fa3c7255c3e88683ea844943
esat <esat@muhammeds-MacBook-Pro.local>
2017-02-12 17:10:41 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ScriptBot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ScriptBot.dir/main.cpp.o"
+  "ScriptBot.pdb"
+  "ScriptBot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ScriptBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
